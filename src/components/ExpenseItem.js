7338226973e1690dca2f82b04db63edeebd35a3d
BhushanBhale27 <bhushan27official@gmail.com>
2023-04-13 14:51:54 +0530

    ExpenseItem Component 1 Js file

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,15 @@
+//A component is react is just a function.
+// special function because it reaturns JSX
+
+function ExpenseItem(){
+    return (
+    <div>
+        <h2>Expense Item!</h2>
+        <p>Food Rs 10</p>
+        <p>Petrol Rs 100</p>
+        <p>Movies Rs 200</p>
+    </div>
+    );
+};
+
+export default ExpenseItem;
