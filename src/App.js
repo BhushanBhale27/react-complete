5b136f2b9ae76d0e6790a2be7f77e7ec88ac4ef2
BhushanBhale27 <bhushan27official@gmail.com>
2023-04-13 17:59:22 +0530

    map method on component

--- src/App.js
+++ src/App.js
@@ -34,35 +34,51 @@ function App() {
       LocationOfExpenditure: "Saloon",
     },
   ];
+
   return (
     <div className="expense-item">
       <header className="App-header">
         {/* Now add custom HTML component as Capital Letter */}
         {/* Add above data in each expenseItem components */}
-        <ExpenseItem
+
+        {/* Apply map method to iterate each expense */}
+
+        {expenses.map((expense) => {
+           return <ExpenseItem
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+            LocationOfExpenditure={expense.LocationOfExpenditure}
+          ></ExpenseItem>;
+        })}
+
+        {/* <ExpenseItem
           title={expenses[0].title}
           amount={expenses[0].amount}
           date={expenses[0].date}
           LocationOfExpenditure={expenses[0].LocationOfExpenditure}
         ></ExpenseItem>
+
         <ExpenseItem
           title={expenses[1].title}
           amount={expenses[1].amount}
           date={expenses[1].date}
           LocationOfExpenditure={expenses[1].LocationOfExpenditure}
         ></ExpenseItem>
+
         <ExpenseItem
           title={expenses[2].title}
           amount={expenses[2].amount}
           date={expenses[2].date}
           LocationOfExpenditure={expenses[2].LocationOfExpenditure}
         ></ExpenseItem>
+
         <ExpenseItem
           title={expenses[3].title}
           amount={expenses[3].amount}
           date={expenses[3].date}
           LocationOfExpenditure={expenses[3].LocationOfExpenditure}
-        ></ExpenseItem>
+        ></ExpenseItem> */}
       </header>
     </div>
   );
