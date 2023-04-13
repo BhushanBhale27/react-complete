4a09c3ffc7f7dd265a8803a533cca181b1323e5b
BhushanBhale27 <bhushan27official@gmail.com>
2023-04-13 18:01:27 +0530

    using props linked with map method of App.js

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,30 @@
 //A component is react is just a function.
 // special function because it reaturns JSX
+import "./ExpenseItem.css";
 
-function ExpenseItem(){
-    return (
-    <div>
-        <h2>Expense Item!</h2>
-        <p>Food Rs 10</p>
-        <p>Petrol Rs 100</p>
-        <p>Movies Rs 200</p>
+function ExpenseItem(props) {
+  //use js variables
+
+  // no use of below variables because props
+  // const expenseDate = new Date(2023, 4, 13);
+  //   const expenseTitle1 = "Food";
+  //   const expenseTitle2 = "Milk";
+  //   const expenseTitle3 = "Petrol";
+  //   const expenseAmount1 = 10;
+  //   const expenseAmount2 = 100;
+  //   const expenseAmount3 = 250;
+  //   const LocationOfExpenditure = "School";
+
+  return (
+    <div className="expense-item">
+      <div>{props.date.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2 className="expense-item">{props.title}</h2>
+        <div className="expense-item__price ">{props.amount}</div>
+        <h2>{props.LocationOfExpenditure}</h2>
+      </div>
     </div>
-    );
-};
+  );
+}
 
 export default ExpenseItem;
