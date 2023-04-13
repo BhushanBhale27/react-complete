70e25cd12e85ed19846fe04037a4426e2bc8c237
BhushanBhale27 <bhushan27official@gmail.com>
2023-04-13 17:30:13 +0530

    Passing Data assignment task using props method

--- src/App.js
+++ src/App.js
@@ -1,14 +1,68 @@
 // import logo from './logo.svg';
-import './App.css';
-import ExpenseItem from './components/ExpenseItem';
+// import './App.css';
+import ExpenseItem from "./components/ExpenseItem";
 
 //Here I comment out react default code just
 
 function App() {
+  const expenses = [
+    {
+      title: "Food",
+      amount: 10,
+      date: new Date(2023, 4, 13),
+      LocationOfExpenditure: "School",
+    },
+
+    {
+      title: "milk",
+      amount: 65,
+      date: new Date(2023, 4, 11),
+      LocationOfExpenditure: "Kojagiri",
+    },
+
+    {
+      title: "petrol",
+      amount: 120,
+      date: new Date(2023, 4, 10),
+      LocationOfExpenditure: "PetrolPump",
+    },
+
+    {
+      title: "Hair",
+      amount: 1000,
+      date: new Date(2023, 4, 11),
+      LocationOfExpenditure: "Saloon",
+    },
+  ];
   return (
-    <div className="App">
+    <div className="expense-item">
       <header className="App-header">
-        <ExpenseItem></ExpenseItem>
+        {/* Now add custom HTML component as Capital Letter */}
+        {/* Add above data in each expenseItem components */}
+        <ExpenseItem
+          title={expenses[0].title}
+          amount={expenses[0].amount}
+          date={expenses[0].date}
+          LocationOfExpenditure={expenses[0].LocationOfExpenditure}
+        ></ExpenseItem>
+        <ExpenseItem
+          title={expenses[1].title}
+          amount={expenses[1].amount}
+          date={expenses[1].date}
+          LocationOfExpenditure={expenses[1].LocationOfExpenditure}
+        ></ExpenseItem>
+        <ExpenseItem
+          title={expenses[2].title}
+          amount={expenses[2].amount}
+          date={expenses[2].date}
+          LocationOfExpenditure={expenses[2].LocationOfExpenditure}
+        ></ExpenseItem>
+        <ExpenseItem
+          title={expenses[3].title}
+          amount={expenses[3].amount}
+          date={expenses[3].date}
+          LocationOfExpenditure={expenses[3].LocationOfExpenditure}
+        ></ExpenseItem>
       </header>
     </div>
   );
