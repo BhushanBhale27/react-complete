16a53292d9092237c4b0bc5494925ccc61d68b88
BhushanBhale27 <bhushan27official@gmail.com>
2023-04-14 09:30:58 +0530

    After component splitting

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -2,9 +2,15 @@
 // special function because it reaturns JSX
 import "./ExpenseItem.css";
 
+import ExpenseDate from "./ExpenseDate";
+import ExpenseDetails from "./ExpenseDetails";
+
 function ExpenseItem(props) {
+  // Way 3 using props
+
   //use js variables
 
+  // Way 2:-
   // no use of below variables because props
   // const expenseDate = new Date(2023, 4, 13);
   //   const expenseTitle1 = "Food";
@@ -15,14 +21,35 @@ function ExpenseItem(props) {
   //   const expenseAmount3 = 250;
   //   const LocationOfExpenditure = "School";
 
+
+  // we make seperate component for this hence commented out
+  // const month = props.date.toLocaleString("en-US", { month: "long" })
+  // const day = props.date.toLocaleString("en-US",{day:"2-digit"});
+  // const year = props.date.getFullYear();
+
   return (
     <div className="expense-item">
-      <div>{props.date.toISOString()}</div>
-      <div className="expense-item__description">
+      {/* <div> */}
+        {/* ABOVE is main div for Date WE NEED TO MAKE SEPERATE COMPONENT FOR THIS DATE DIV*/}
+        {/* <div>{day}</div> div for day */}
+        {/* <div>{month}</div>
+        <div>{year}</div>
+      </div> */}
+
+      {/* <ExpenseDate></ExpenseDate>  */}
+
+      {/* OR if we not write content between this component tag then we can use it as BELOW*/}
+
+      <ExpenseDate date={props.date}/>
+
+      {/* <div className="expense-item__description">
         <h2 className="expense-item">{props.title}</h2>
         <div className="expense-item__price ">{props.amount}</div>
         <h2>{props.LocationOfExpenditure}</h2>
-      </div>
+      </div> */}
+  
+      {/* we split above code into new component */}
+      <ExpenseDetails title = {props.title} amount={props.amount} LocationOfExpenditure={props.LocationOfExpenditure} />
     </div>
   );
 }
