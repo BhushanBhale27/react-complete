62421c13398b85b3c7344c342b5479f4599cf6d0
BhushanBhale27 <bhushan27official@gmail.com>
2023-04-15 08:48:36 +0530

    Added delete button

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -7,53 +7,33 @@ import ExpenseDate from "../Expenses/ExpenseDate";
 import ExpenseDetails from "../Expenses/ExpenseDetails";
 
 // function ExpenseItem(props) { OR we can use arrow function
- const ExpenseItem = (props) => {
-  // Way 3 using props
-
-  //use js variables
-
-  // Way 2:-
-  // no use of below variables because props
-  // const expenseDate = new Date(2023, 4, 13);
-  //   const expenseTitle1 = "Food";
-  //   const expenseTitle2 = "Milk";
-  //   const expenseTitle3 = "Petrol";
-  //   const expenseAmount1 = 10;
-  //   const expenseAmount2 = 100;
-  //   const expenseAmount3 = 250;
-  //   const LocationOfExpenditure = "School";
-
-
-  // we make seperate component for this hence commented out
-  // const month = props.date.toLocaleString("en-US", { month: "long" })
-  // const day = props.date.toLocaleString("en-US",{day:"2-digit"});
-  // const year = props.date.getFullYear();
+const ExpenseItem = (props) => {
+     {/* WAY 2  */}
+ const clickHandler = () => {
+  window.alert("Clicked!!!")
+ }
 
   return (
     <Card className="expense-item">
-      {/* <div> */}
-        {/* ABOVE is main div for Date WE NEED TO MAKE SEPERATE COMPONENT FOR THIS DATE DIV*/}
-        {/* <div>{day}</div> div for day */}
-        {/* <div>{month}</div>
-        <div>{year}</div>
-      </div> */}
 
-      {/* <ExpenseDate></ExpenseDate>  */}
+     <ExpenseDate date={props.date} />
 
-      {/* OR if we not write content between this component tag then we can use it as BELOW*/}
+      <ExpenseDetails
+        title={props.title}
+        amount={props.amount}
+        LocationOfExpenditure={props.LocationOfExpenditure}
+      />
+      {/*  WAY 1 (not used)<button onClick={()=>{window.alert('Clicked!!')}}>Change Text</button> */}
 
-      <ExpenseDate date={props.date}/>
-
-      {/* <div className="expense-item__description">
-        <h2 className="expense-item">{props.title}</h2>
-        <div className="expense-item__price ">{props.amount}</div>
-        <h2>{props.LocationOfExpenditure}</h2>
-      </div> */}
-  
-      {/* we split above code into new component */}
-      <ExpenseDetails title = {props.title} amount={props.amount} LocationOfExpenditure={props.LocationOfExpenditure} />
+      {/* WAY 2  */}
+      <button
+        onClick={clickHandler}
+      >
+        Change Text
+      </button>
+      <button>Delete Expense</button>
     </Card>
   );
-}
+};
 
 export default ExpenseItem;
