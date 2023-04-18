10d2abad6558e2c4319c96ae2a53a8691a8809e8
BhushanBhale27 <bhushan27official@gmail.com>
2023-04-18 11:51:18 +0530

    Conditional rendering all ways

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -5,16 +5,28 @@ import Card from "../UI/Card";
 import "../Expenses/Expenses.css";
 
 const Expenses = (props) => {
-  const [filteredYear, setFilteredYear] = useState("2020");
+  const [filteredYear, setFilteredYear] = useState("2023");
 
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
   };
- 
-  const FilteredExpenses = props.items.filter(expense=>{
-    return expense.date.getFullYear().toString() === filteredYear
-  })
 
+  const FilteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+  // Way 3 of conditional rebdering
+  // let expensesCount = <p>No expenses found</p>;
+
+  // if (FilteredExpenses.length > 0) {
+  //   expensesCount = FilteredExpenses.map((expense) => (
+  //     <ExpenseItem
+  //       key={expense.id}
+  //       title={expense.title}
+  //       amount={expense.amount}
+  //       date={expense.date}
+  //     />
+  //   ));
+  // }
 
   return (
     <div>
@@ -24,15 +36,23 @@ const Expenses = (props) => {
           onChangeFilter={filterChangeHandler}
         />
 
-        {FilteredExpenses.map((expense) => (
-          <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        ))}
+        {/* method 1 of conditional rendering using ternary operator */}
+        {FilteredExpenses.length === 1 ? (
+          <p>Only single Expense here. Please add more...</p>
+        ) : (
+          FilteredExpenses.map((expense) => (
+            <ExpenseItem
+              key={expense.id}
+              title={expense.title}
+              amount={expense.amount}
+              date={expense.date}
+            />
+          ))
+        )}
 
+        {/* method 2 of conditional rendering using && USE THIS METHOD */}
+
+        {/* {expensesCount} */}
       </Card>
     </div>
   );
